8588ab6b293bdd138047598af1662b039bb6a309
MichelleCordeiro <mic321@gmail.com>
2022-09-17 17:16:21 -0300

    #12: mobile - routes done

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 
-import { Home } from './src/screens/Home'
+import { Routes } from './src/routes'
 import { Loading } from './src/components/Loading'
 import { Background } from './src/components/Background'
 
@@ -22,8 +22,7 @@ export default function App() {
   return (
     <Background>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   )
 }
-// 1:38:00
